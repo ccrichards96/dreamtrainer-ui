a93972e1fe5734dfa784fc9a85b184b60b8928c2
Chris Richards <chris@myrelaytech.com>
2025-08-11 16:55:27 -0500

    constants

---
+++ src/constants/strings.ts
@@ -0,0 +1,11 @@
+export default {
+    dashboard: {
+        TITLE: "Dashboard",
+        SUBTITLE: "Let's get you started with your professional certification journey.",
+        HELP_LINK: "https://calendly.com/notefulljoseph/toefl-course-help"
+    },
+    onboarding: {
+        TITLE: "Welcome to DreamTrainer",
+        SUBTITLE: "Your personalized TOEFL preparation journey starts here!",
+    },
+}
