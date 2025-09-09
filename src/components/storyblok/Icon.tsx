6b7acc6c4242495d0a068df643e13eb1835e5577
Chris Richards <chris@myrelaytech.com>
2025-09-08 21:18:10 -0500

    icon typing

--- src/components/storyblok/Icon.tsx
+++ src/components/storyblok/Icon.tsx
@@ -5,29 +5,36 @@ import * as SimpleIcons from "react-icons/si";
 import * as BootrapIcons from "react-icons/bs";
 import * as PiIcons from "react-icons/pi";
 
-const IconComponent = ({ blok }) => {
-  let ic: any = null;
-  let icon: any = null;
+interface IconBlok {
+  name: string;
+  iconLib: string;
+  size: string | number;
+  color: string;
+}
+
+const IconComponent = ({ blok }: { blok: IconBlok }) => {
+  let ic: React.ComponentType | null = null;
+  let icon: React.ReactElement | null = null;
   if (blok.name !== "") {
     try {
       if (blok.iconLib === "fa") {
-        ic = FontAwesome[blok.name];
-        icon = React.createElement(ic);
+        ic = FontAwesome[blok.name as keyof typeof FontAwesome];
+        icon = ic ? React.createElement(ic) : null;
       } else if (blok.iconLib === "di") {
-        ic = DevIcons[blok.name];
-        icon = React.createElement(ic);
+        ic = DevIcons[blok.name as keyof typeof DevIcons];
+        icon = ic ? React.createElement(ic) : null;
       } else if (blok.iconLib === "si") {
-        ic = SimpleIcons[blok.name];
-        icon = React.createElement(ic);
+        ic = SimpleIcons[blok.name as keyof typeof SimpleIcons];
+        icon = ic ? React.createElement(ic) : null;
       } else if (blok.iconLib === "bs") {
-        ic = BootrapIcons[blok.name];
-        icon = React.createElement(ic);
+        ic = BootrapIcons[blok.name as keyof typeof BootrapIcons];
+        icon = ic ? React.createElement(ic) : null;
       } else if (blok.iconLib === "pi") {
-        ic = PiIcons[blok.name];
-        icon = React.createElement(ic);
+        ic = PiIcons[blok.name as keyof typeof PiIcons];
+        icon = ic ? React.createElement(ic) : null;
       } else {
-        ic = FontAwesome[blok.name];
-        icon = React.createElement(ic);
+        ic = FontAwesome[blok.name as keyof typeof FontAwesome];
+        icon = ic ? React.createElement(ic) : null;
       }
     } catch (e) {
       console.log(e);
